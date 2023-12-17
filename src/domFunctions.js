b00fc56232de7ad0f81505cd46ffdcbe0c5c5e75
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2023-12-17 09:23:11 +0100

    Add text to speech functions

--- src/domFunctions.js
+++ src/domFunctions.js
@@ -9,7 +9,13 @@ function createEventListeners() {
     copySourceButton.addEventListener('click', copySourceText);
 
     const copyTranslationButton = document.getElementById('copy-translation');
-    copyTranslationButton.addEventListener('click', copyTranslation)
+    copyTranslationButton.addEventListener('click', copyTranslation);
+
+    const speakSourceTextButton = document.getElementById('speak-source-text');
+    speakSourceTextButton.addEventListener('click', speakSourceText);
+
+    const speakTranslationButton = document.getElementById('speak-translation');
+    speakTranslationButton.addEventListener('click', speakTranslation)
 }
 
 
@@ -30,6 +36,18 @@ function copyTranslation() {
     copyTextarea.select();
 
     document.execCommand('copy');
+}
+
+function speakSourceText() {
+    const textToSpeak = document.getElementById('text-to-translate').value;
+    const utterance = new SpeechSynthesisUtterance(textToSpeak);
+    window.speechSynthesis.speak(utterance);
+}
+
+function speakTranslation() {
+    const textToSpeak = document.getElementById('translation-area').value;
+    const utterance = new SpeechSynthesisUtterance(textToSpeak);
+    window.speechSynthesis.speak(utterance);
 }
 
 export { createEventListeners, showTranslation }
